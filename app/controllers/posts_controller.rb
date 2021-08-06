b1a3d2427d44182efc2fd7c84889e0203c2b9fd3
WadaAtsushi <tsurupika0901@.gmail.com>
2021-08-07 01:19:13 +0900

    posts　テストデータ入力

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,28 +7,59 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  # def create
+  #   @post = @current_menbur.posts.build(post_params)
+  #   @post.user_id = @current_user.id
+  #   @post.nancyatterapp_id =  @current_app.id
+
+  #   if @post.save
+  #     flash.notice = "投稿しました。"
+  #     redirect_to post_index_path
+  #   else
+  #     flash.notice = "投稿失敗しました。"
+  #     render('posts/new')
+  #   end
+  # end
+
   def create
-    @post = @current_menbur.posts.build(post_params)
-    @post.user_id = @current_user.id
-    @post.nancyatterapp_id =  @current_app.id
+    user_session = session[:user_id]
+    menbur_session = session[:menbur_id]
+    app_session = session[:nancyatterapp_id]
 
+    (1..300).each do |i|
+        test_menbur = Menbur.all.sample
+        test_user = test_menbur.user
+        test_app = test_menbur.nancyatterapp
+        test_category = test_app.postcategories.all.sample
 
-    p "=========="
-    p post_params
-    p "==========="
-    p @post
-    p "=========="
-    if @post.save
-      flash.notice = "投稿しました。"
-      redirect_to post_index_path
-    else
-      flash.notice = "投稿失敗しました。"
-      render('posts/new')
+        session[:user_id] = test_user.id
+        session[:menbur_id] = test_menbur.id
+        session[:nancyatterapp_id] = test_app.id
+
+        test_post = Post.new(
+            title: "post#{i}", 
+            content: "psot#{i}_content#{i}_hogefugahogefuga", 
+            post_image: params[:post][:post_image],
+            user_id: session[:user_id],
+            menbur_id: session[:menbur_id],
+            postcategory_id: test_category.id,
+            nancyatterapp_id: session[:nancyatterapp_id]
+        )
+        
+        test_post.save
+
     end
-  end
+    session[:user_id] = user_session
+    session[:menbur_id] = menbur_session
+    session[:nancyatterapp_id] = app_session
+
+    flash.notice = "投稿しました"
+    redirect_to post_index_path
+end
+
 
   def index
-    @posts = Post.all.page(params[:page]).per(10)
+    @posts = Post.where(nancyatterapp_id: @current_app.id).all.page(params[:page]).per(10)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,102 +15,112 @@ p " ====== user テストデータ作成 =======================================
     
 
 p " ====== nancyatterapp テストデータ作成 ====================================="
-# *** nancyatterコントローラに貼り付けて実行 ***
+*** nancyatterコントローラに貼り付けて実行 ***
 
-# def create
-#     user_session = session[:user_id]
-#     (1..10).each do |i|
-#         test_user = User.all.sample
-#         appcategory = Appcategory.all.sample
-#         session[:user_id] = test_user.id
+def create
+    user_session = session[:user_id]
+    (1..10).each do |i|
+        test_user = User.all.sample
+        appcategory = Appcategory.all.sample
+        session[:user_id] = test_user.id
 
-#         test_app = Nancyatterapp.new(
-#             app_name: "test_app#{i}",
-#             main_text: "これはテストアプリ#{i}です。  ===test#{i}===",
-#             introduction: "テストアプリ#{i}の紹介文です。  ===test#{i}===",
-#             loure: "テストアプリ#{i}の使用ルールです。  ===test#{i}===",
-#             app_image: params[:nancyatterapp][:app_image],
-#             user_id: session[:user_id],
-#             appcategory_id: appcategory.id,
-#             feature_like: "implement",
-#             feature_comment: "implement",
-#             feature_category: "implement",
-#             feature_image: "implement"
-#         )
-#         p "========="
-#         p test_user
-#         p test_app
-#         p "========="
+        test_app = Nancyatterapp.new(
+            app_name: "test_app#{i}",
+            main_text: "これはテストアプリ#{i}です。  ===test#{i}===",
+            introduction: "テストアプリ#{i}の紹介文です。  ===test#{i}===",
+            loure: "テストアプリ#{i}の使用ルールです。  ===test#{i}===",
+            app_image: params[:nancyatterapp][:app_image],
+            user_id: session[:user_id],
+            appcategory_id: appcategory.id,
+            feature_like: "implement",
+            feature_comment: "implement",
+            feature_category: "implement",
+            feature_image: "implement"
+        )
+        p "========="
+        p test_user
+        p test_app
+        p "========="
 
-#         test_app.save
-#     end
-#     session[:user_id] = user_session
-#     redirect_to root_path
-# end
+        test_app.save
+    end
+    session[:user_id] = user_session
+    redirect_to root_path
+end
 
 
 p " ====== menbur テストデータ作成 ====================================="
-# *** menbur コントローラに貼り付けて実行 ***
+*** menbur コントローラに貼り付けて実行 ***
 
-# def create
-#     user_session = session[:user_id]
-#     app_session = session[:nancyatterapp_id]
-#     (1..50).each do |i|
-#         test_user = User.all.sample
-#         session[:user_id] = test_user.id
-#         test_app = Nancyatterapp.all.sample
-#         session[:nancyatterapp_id] = test_app.id
+def create
+    user_session = session[:user_id]
+    app_session = session[:nancyatterapp_id]
+    (1..50).each do |i|
+        test_user = User.all.sample
+        session[:user_id] = test_user.id
+        test_app = Nancyatterapp.all.sample
+        session[:nancyatterapp_id] = test_app.id
 
-#         test_menbur = Menbur.new(
-#             nickname: "menbur#{i}",
-#             profile: "menbur#{i}のテストプロフィール。===test#{i}===",
-#             profile_image: params[:menbur][:profile_image],
-#             nancyatterapp_id: session[:nancyatterapp_id],
-#             user_id: session[:user_id]
-#         )
-#         p "========="
-#         p test_user
-#         p test_app
-#         p test_menbur
-#         p "========="
+        test_menbur = Menbur.new(
+            nickname: "menbur#{i}",
+            profile: "menbur#{i}のテストプロフィール。===test#{i}===",
+            profile_image: params[:menbur][:profile_image],
+            nancyatterapp_id: session[:nancyatterapp_id],
+            user_id: session[:user_id]
+        )
+        p "========="
+        p test_user
+        p test_app
+        p test_menbur
+        p "========="
 
-#         test_menbur.save
-#         session[:menbur_id] = test_menbur.id
-#     end
-#     session[:user_id] = user_session
-#     session[:nancyatterapp_id] = app_session
-#     redirect_to root_path
-# end
+        test_menbur.save
+        session[:menbur_id] = test_menbur.id
+    end
+    session[:user_id] = user_session
+    session[:nancyatterapp_id] = app_session
+    redirect_to root_path
+end
 
 
-p "====== menbur テストデータ作成 ====================================="
+p "====== post テストデータ作成 ====================================="
+# *** posts コントローラに貼り付けて実行 ***
 
-# def create
-#     user_session = session[:user_id]
-#     (1..5).each do |i|
-#         test_user = User.find_by(name: "user#{rand(1..10)}")
-#         category = Category.find_by(post_category: "post_category_#{rand(1..5)}")
+def create
+    user_session = session[:user_id]
+    menbur_session = session[:menbur_id]
+    app_session = session[:nancyatterapp_id]
+
+    (1..300).each do |i|
+        test_menbur = Menbur.all.sample
+        test_user = test_menbur.user
+        test_app = test_menbur.nancyatterapp
+        test_category = test_app.postcategories.all.sample
+
+        session[:user_id] = test_user.id
+        session[:menbur_id] = test_menbur.id
+        session[:nancyatterapp_id] = test_app.id
+
+        test_post = Post.new(
+        title: "post#{i}", 
+        content: "psot#{i}_content#{i}_hogefugahogefuga", 
+        post_image: params[:post][:post_image],
+        user_id: session[:user_id],
+        menbur_id: session[:menbur_id],
+        postcategory_id: test_category.id,
+        nancyatterapp_id: session[:nancyatterapp_id]
+        )
         
-#         session[:user_id] = test_user.id
+        test_post.save
 
-#         test_post = Post.new(
-#         title: "post#{i}", 
-#         content: "psot#{i}_content#{i}_hogefugahogefuga", 
-#         image: params[:post][:post_image],
-#         menbur_id: session[:menbur_id],
-#         category_id: category.id
-#         )
-#         p "========="
-#         p test_post
-#         p "========="
+    end
+    session[:user_id] = user_session
+    session[:menbur_id] = menbur_session
+    session[:nancyatterapp_id] = app_session
 
-#         test_post.save
-#     end
-#     session[:user_id] = user_session
-
-#     flash.notice = "投稿しました"
-#     redirect_to post_index_path
-# end
+    flash.notice = "投稿しました"
+    redirect_to post_index_path
+end
 
 p "====== psotcategory テストデータ作成 ====================================="
 
